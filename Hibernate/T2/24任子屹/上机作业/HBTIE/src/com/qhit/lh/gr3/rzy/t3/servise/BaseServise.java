9d0165179d59ea0eff6201814c7d62241183144f
RenZiYi1997 <374245616@qq.com>
2017-12-14 12:43:40 +0800

    24，任子屹提交 Hibernate T02上机作业

---
+++ Hibernate/T2/24任子屹/上机作业/HBTIE/src/com/qhit/lh/gr3/rzy/t3/servise/BaseServise.java
@@ -0,0 +1,27 @@
+package com.qhit.lh.gr3.rzy.t3.servise;
+
+import java.util.List;
+
+public interface BaseServise {
+	/**
+	 * 增
+	 */
+public void add(Object obj);
+/**
+ * 删
+ */
+public void delect(Object obj);
+/**
+ * 改
+ */
+public void update(Object obj);
+/**
+ * 查
+ */
+public List<Object> getAll(String fromObject);
+/**
+ * 根据ID去查
+ */
+public Object getObjectById(Object obj, int id);
+
+}
---
+++ Hibernate/T2/24任子屹/上机作业/HBTIE/src/test/UserTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.qhit.lh.gr3.rzy.t3.bean.User;
+
+public class UserTest {
+
+	@Test
+	public void add() {
+		//声明并实例化对象
+		User user = new User();
+		//添加数据
+		
+		
+	}
+	@Test
+	public void delete() {
+		
+	}
+	@Test
+	public void update() {
+		
+	}
+	@Test
+	public void getAll() {
+		
+	}
+}
